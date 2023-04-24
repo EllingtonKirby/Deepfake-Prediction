c555e048e24686499f2be8ff945f5ba9984d0ce6
Ellington <ellingtonkirby31@gmail.com>
2023-04-24 14:28:50 +0200

    preprocess to remove numbers

--- ek/dataset_preprocessor.py
+++ ek/dataset_preprocessor.py
@@ -1,30 +1,41 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 import sys
-sys.path.append('/Users/ellington/dev/preprocessor/')
+sys.path.append('/Users/ellington/dev/preprocessor/')  # nopep8
 import preprocessor as p
+
 
 def tokenize_emojis_links_and_compare_counts(filename, data,):
     # Vectorize input
     input_vectorizer = CountVectorizer()
     input_vectorizer.fit_transform(data)
-    print(f"Before tokenizing, total token count is: {len(input_vectorizer.get_feature_names_out())}")
+    print(
+        f"Before tokenizing, total token count is: {len(input_vectorizer.get_feature_names_out())}")
 
     with open(filename, 'w') as f:
         data.to_json(f, orient='values')
-    
-    p.api.set_options(p.OPT.URL, p.OPT.EMOJI, p.OPT.MENTION, p.OPT.HASHTAG, p.OPT.SMILEY, p.OPT.NUMBER)
-    cleaned_output = p.api.tokenize_file(filename)
-    cleaned_data = pd.read_json(cleaned_output, typ='series')
+
+    print("Cleaning file: removing numbers")
+    p.api.set_options(p.OPT.NUMBER)
+    cleaned_output = p.api.clean_file(filename)
+
+    print("Cleaning file: tokenizing urls, emojis, mentions, hashtags, and smileys")
+    p.api.set_options(p.OPT.URL, p.OPT.EMOJI, p.OPT.MENTION,
+                      p.OPT.HASHTAG, p.OPT.SMILEY)
+    tokenized_output = p.api.tokenize_file(cleaned_output)
+    tokenized_data = pd.read_json(tokenized_output, typ='series')
 
     # Vectorize output
     output_vectorizer = CountVectorizer()
-    output_vectorizer.fit_transform(cleaned_data)
-    print(f"After tokenizing, total token count is: {len(output_vectorizer.get_feature_names_out())}")
+    output_vectorizer.fit_transform(tokenized_data)
+    print(
+        f"After tokenizing, total token count is: {len(output_vectorizer.get_feature_names_out())}")
 
-    return cleaned_data
+    return tokenized_data
+
 
 def confirm_veectorized_word_count(data):
     output_vectorizer = CountVectorizer()
     output_vectorizer.fit_transform(data)
-    print(f"After cleaning, total token count is: {len(output_vectorizer.get_feature_names_out())}")
+    print(
+        f"After cleaning, total token count is: {len(output_vectorizer.get_feature_names_out())}")
